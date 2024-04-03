3c25b2fd123d97ff018aefdba6c534370239be50
Pruszyn5112 <pruszynbusiness@gmail.com>
2024-04-03 19:56:24 +0200

    Add project files.

---
+++ Movie-collection-app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Movie_collection_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
